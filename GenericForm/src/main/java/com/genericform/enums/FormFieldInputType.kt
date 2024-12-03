de88afe48210068b802325f2e6db45bc80fa936a
Kunal Singhal <97699627+kanha-jii@users.noreply.github.com>
2024-12-03 10:16:32 +0530

    Update FormFieldInputType.kt

--- GenericForm/src/main/java/com/genericform/enums/FormFieldInputType.kt
+++ GenericForm/src/main/java/com/genericform/enums/FormFieldInputType.kt
@@ -1,24 +1,26 @@
 package com.genericform.enums
 
+import androidx.compose.ui.Modifier
+
 //enum class FormFieldInputType {
 //    TEXT, NUMBER, EMAIL, PASSWORD, DATE, CARDNUMBER, MONTHDROPDOWN, YEARDROPDOWN, CUSTOM, CUSTOMDROPDOWN
 //}
 
 sealed class FormFieldInputType {
-    data class Text(val text:String = "") : FormFieldInputType()
-    data object NUMBER : FormFieldInputType()
-    data class Email(val emailText:String = "") : FormFieldInputType()
-    data object PASSWORD : FormFieldInputType()
-    data object DATE : FormFieldInputType()
-    data class CardNumber(val cardText:String = "") : FormFieldInputType()
-    data class MonthDropDown(val initialSelectedMonth:String = "") : FormFieldInputType()
-    data class YearDropDown(val initialSelectedYear:String = "") : FormFieldInputType()
-    data class PickImage(val url:String = "") : FormFieldInputType()
+    data class Text(val text:String = "", val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class Number(val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class Email(val emailText:String = "", val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class Password(val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class Date(val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class CardNumber(val cardText:String = "", val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class MonthDropDown(val initialSelectedMonth:String = "", val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class YearDropDown(val initialSelectedYear:String = "", val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class PickImage(val url:String = "",val placeHolder:Int? = null, val modifier: Modifier = Modifier) : FormFieldInputType()
     // TODO: spell check text area, restrict particular words text area
-    data class TextArea(val textAreaText:String = "") : FormFieldInputType()
-    data object CUSTOM : FormFieldInputType()
-    data class CheckBox(val fieldName:String,val options: List<String>,val initialSelectedOption:String = "") : FormFieldInputType()
-    data class RadioButton(val fieldName:String,val options: List<String>,val initialSelectedOption:String = "") : FormFieldInputType()
-    data class CustomDropDown(val fieldName:String,val options: List<String>,val initialSelectedOption:String = "") : FormFieldInputType()
+    data class TextArea(val textAreaText:String = "", val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class Custom(val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class CheckBox(val fieldName:String,val options: List<String>,val initialSelectedOption:String = "", val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class RadioButton(val fieldName:String,val options: List<String>,val initialSelectedOption:String = "", val modifier: Modifier = Modifier) : FormFieldInputType()
+    data class CustomDropDown(val fieldName:String,val options: List<String>,val initialSelectedOption:String = "", val modifier: Modifier = Modifier) : FormFieldInputType()
     data class StepperControl(val textInMid:Boolean) : FormFieldInputType()
 }
